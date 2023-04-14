e381f49418ac62bae8cdcc882845c4cac108e5d0
ChrisCedi <christian.cedillo.palacios@outlook.com>
2023-04-14 00:37:48 -0600

    feature: axios configuration

--- src/App.js
+++ src/App.js
@@ -1,5 +1,24 @@
+import { Button } from "@mui/material";
+import { marvelApi } from "./api/marvelApi";
+
 function App() {
-  return <div className="App">hola a todos</div>;
+  const prueba = async () => {
+    const response = await marvelApi.get(
+      `characters?ts=3&apikey=717fc2d7beae7cd0e3c30ad545d2597a&hash=0c65084a88fbfc7fb9b477ffea85b1a8`
+    );
+  };
+
+  // useEffect(() => {
+  //   prueba();
+  // }, []);
+
+  return (
+    <div className="App">
+      <Button variant="contained" onClick={prueba}>
+        consumir
+      </Button>
+    </div>
+  );
 }
 
 export default App;
---
+++ src/api/config.js
@@ -0,0 +1,2 @@
+export const PUBLIC = "717fc2d7beae7cd0e3c30ad545d2597a";
+export const PASSWORD = "0c65084a88fbfc7fb9b477ffea85b1a8";
