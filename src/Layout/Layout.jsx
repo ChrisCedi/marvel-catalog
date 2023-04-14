7bfb2ac9259fe91681f5685e5cd53895eb1f2e3c
ChrisCedi <christian.cedillo.palacios@outlook.com>
2023-04-14 04:48:03 -0600

    feature: hover effect in cards and marvel logo

--- src/Layout/Layout.jsx
+++ src/Layout/Layout.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { Container } from "@material-ui/core";
 import { useStyles } from "./LayoutStyles";
 import { Header } from "../components/Header/Header";
+import { motion } from "framer-motion";
 
 export const Layout = ({ children }) => {
   const classes = useStyles();
@@ -10,11 +11,13 @@ export const Layout = ({ children }) => {
     <>
       <Header />
       <div className={classes.mainDiv}>
-        <img
-          src="https://upload.wikimedia.org/wikipedia/commons/thumb/b/b9/Marvel_Logo.svg/1200px-Marvel_Logo.svg.png"
-          alt="marvel"
-          className={classes.logo}
-        />
+        <motion.div whileHover={{ scale: 1.2 }}>
+          <img
+            src="https://upload.wikimedia.org/wikipedia/commons/thumb/b/b9/Marvel_Logo.svg/1200px-Marvel_Logo.svg.png"
+            alt="marvel"
+            className={classes.logo}
+          />
+        </motion.div>
       </div>
       <Container className={classes.body}>{children}</Container>
     </>
