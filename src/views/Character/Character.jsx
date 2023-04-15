4bde20384e2b24cf036d43e4a2670d04d64d6e58
ChrisCedi <christian.cedillo.palacios@outlook.com>
2023-04-15 17:02:12 -0600

    feature: character view

--- src/views/Character/Character.jsx
+++ src/views/Character/Character.jsx
@@ -2,7 +2,8 @@ import React, { useEffect } from "react";
 import { useStyles } from "./CharacterStyles";
 import { useParams } from "react-router-dom";
 import { useMarvel } from "../../components/MarvelProvider/hooks/useMarvel";
-import { Typography, Grid } from "@material-ui/core";
+import { Typography, Grid, CardActionArea } from "@material-ui/core";
+import { motion } from "framer-motion";
 
 export const Character = () => {
   const classes = useStyles();
@@ -12,6 +13,7 @@ export const Character = () => {
   useEffect(() => {
     getCharacterById(id);
     getComics(id);
+    window.scroll(0, 0);
   }, []);
 
   return (
@@ -19,20 +21,54 @@ export const Character = () => {
       <Typography className={classes.title} variant="h3">
         {characterInfo.name}
       </Typography>
-      <img
-        src={characterInfo.avatar}
-        alt="character"
-        className={classes.imageAvatar}
-      />
-      <Grid container spacing={5}>
+      <Grid container spacing={4}>
+        <Grid item xs={12} sm={6} md={4}>
+          <img
+            src={characterInfo.avatar}
+            alt="character"
+            className={classes.imageAvatar}
+          />
+        </Grid>
+        <Grid item xs={12} sm={6} md={8} className={classes.gridInfo}>
+          <Typography variant="h5" className={classes.description}>
+            {characterInfo.description}
+          </Typography>
+        </Grid>
+        <Grid item xs={12}>
+          <Typography className={classes.subtitle} variant="h4">
+            Comics
+          </Typography>
+          <Typography variant="h6">
+            Si deseas ver más información sobre un comic dale click y te llevará
+            a la página oficial
+          </Typography>
+        </Grid>
+
         {comicList.map((comic, index) => (
-          <Grid item xs={12} sm={6} md={4} lg={3} key={index}>
-            <img
-              src={comic.comicImage}
-              alt={comic.name}
-              className={classes.comicImage}
-            />
-            <Typography>{comic.name}</Typography>
+          <Grid
+            item
+            xs={12}
+            sm={6}
+            md={4}
+            lg={3}
+            key={index}
+            className={classes.gridComics}
+          >
+            <motion.div
+              whileHover={{ scale: 1.1 }}
+              className={classes.motionImage}
+            >
+              <CardActionArea>
+                <a href={comic.url} className={classes.comicName}>
+                  <img
+                    src={comic.comicImage}
+                    alt={comic.name}
+                    className={classes.comicImage}
+                  />
+                </a>
+              </CardActionArea>
+              <Typography>{comic.name}</Typography>
+            </motion.div>
           </Grid>
         ))}
       </Grid>
--- src/views/Character/CharacterStyles.js
+++ src/views/Character/CharacterStyles.js
@@ -1,21 +1,49 @@
 import { makeStyles } from "@material-ui/core";
 
 const useStyles = makeStyles((theme) => ({
+  container: {
+    paddingBottom: theme.spacing(12),
+  },
   title: {
-    textAlign: "center",
+    textAlign: "left",
+    paddingBottom: theme.spacing(4),
+    textTransform: "upperCase",
+    letterSpacing: theme.spacing(1),
+    [theme.breakpoints.down("xs")]: {
+      fontSize: theme.typography.h5.fontSize,
+    },
+  },
+  description: {
+    [theme.breakpoints.down("xs")]: {
+      fontSize: theme.typography.h6.fontSize,
+    },
+  },
+  subtitle: {
+    textAlign: "left",
   },
   imageAvatar: {
-    width: "50%",
-    height: "50%",
+    width: "100%",
+    height: "350px",
+    borderRadious: theme.spacing(4),
   },
   gridInfo: {
     display: "flex",
-    flexDirection: "column",
-    alignItems: "center",
+    alignItems: "flex-end",
   },
   comicImage: {
     width: "100%",
     height: "400px",
+  },
+  comicName: {
+    textDecoration: "none",
+    color: theme.palette.common.black,
+  },
+  gridComics: {
+    display: "flex",
+    justifyContent: "center",
+  },
+  motionImage: {
+    width: "250px",
   },
 }));
 
