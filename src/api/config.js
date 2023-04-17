ef86d48c279d1da750086479c6441d1b2bc41218
ChrisCedi <christian.cedillo.palacios@outlook.com>
2023-04-17 09:23:45 -0600

    feature: dynamic url

--- src/api/config.js
+++ src/api/config.js
@@ -1,2 +1,2 @@
-export const PUBLIC = "717fc2d7beae7cd0e3c30ad545d2597a";
-export const PASSWORD = "0c65084a88fbfc7fb9b477ffea85b1a8";
+export const PUBLIC_KEY = "717fc2d7beae7cd0e3c30ad545d2597a";
+export const PRIVATE_KEY = "e6712c94cd9e7a791b75fc3a8cac8f4fa8f88fba";
